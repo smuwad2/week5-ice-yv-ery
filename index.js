fac8c96b0feb34d19993f0585a83eec337d8eaa5
Yiap En Rui <enrui.yiap.2024@smu.edu.sg>
2025-09-21 16:47:13 +0000

    ICE wk5

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ app.get('/items', (req, res) => {
     }
     res.json(supermarketItems);
 });
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
